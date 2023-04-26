dea227e73a6ac3120efdba4a2123112d4466b641
Reddy1311 <dhananjayareddy1311@gmail.com>
2023-04-26 07:28:34 +0530

    first commit

---
+++ src/project/Char.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myStr = "Hello";
+		char result = myStr.charAt(0);
+		System.out.println(result);
+	}
+
+}
